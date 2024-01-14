376d7106ab30810802092dd6592c2e36d39ec793
Nadav Galili <67638018+nadav-galili@users.noreply.github.com>
2024-01-14 18:12:47 +0200

    added imahge input

--- App.js
+++ App.js
@@ -1,5 +1,27 @@
+import ImageInput from "./app/components/forms/ImageInput";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
+import Screen from "./app/components/Screen";
+import { Button } from "react-native";
+import React, { useState, useEffect } from "react";
 
 export default function App() {
-  return <ListingEditScreen />;
+  const [imageUri, setImageUri] = useState();
+
+  // const requestPermission = async () => {
+  //   const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
+  //   // if not granted, alert the user
+  //   if (!granted) alert("You need to enable permission to access the library.");
+  // };
+
+  // useEffect(() => {
+  //   requestPermission();
+  // }, []);
+  return (
+    <Screen>
+      <ImageInput
+        imageUri={imageUri}
+        onChangeImage={(uri) => setImageUri(uri)}
+      />
+    </Screen>
+  );
 }
