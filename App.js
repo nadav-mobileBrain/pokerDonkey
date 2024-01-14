0151e07061a1992dcbfce4d2c58f94d04c6b56c2
Nadav Galili <67638018+nadav-galili@users.noreply.github.com>
2024-01-14 18:56:33 +0200

    image list

--- App.js
+++ App.js
@@ -1,26 +1,27 @@
-import ImageInput from "./app/components/forms/ImageInput";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
 import Screen from "./app/components/Screen";
-import { Button } from "react-native";
+
 import React, { useState, useEffect } from "react";
+import ImageInputList from "./app/components/forms/ImageInputList";
 
 export default function App() {
-  const [imageUri, setImageUri] = useState();
+  const [imageUris, setImageUris] = useState([]);
 
-  // const requestPermission = async () => {
-  //   const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
-  //   // if not granted, alert the user
-  //   if (!granted) alert("You need to enable permission to access the library.");
-  // };
+  const handleAdd = (uri) => {
+    setImageUris([...imageUris, uri]);
+  };
 
-  // useEffect(() => {
-  //   requestPermission();
-  // }, []);
+  const handleRemove = (uri) => {
+    setImageUris(imageUris.filter((imageUri) => imageUri !== uri));
+  };
+
   return (
     <Screen>
-      <ImageInput
-        imageUri={imageUri}
-        onChangeImage={(uri) => setImageUri(uri)}
+      <ImageInputList
+        imageUris={imageUris}
+        onAddImage={handleAdd}
+        onRemoveImage={handleRemove}
+        // onRemoveImage={(uri) => setImageUris(imageUris.filter((imageUri) => imageUri !== uri))}
+        // onAddImage={(uri) => setImageUris([...imageUris, uri])}
       />
     </Screen>
   );
