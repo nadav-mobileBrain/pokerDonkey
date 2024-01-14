327d09ec4755e6421419abf73b161b1b72e5b4e6
Nadav Galili <nadavg@mobile-brain.net>
2024-01-14 19:27:01 +0200

    update form

--- App.js
+++ App.js
@@ -1,28 +1,5 @@
-import Screen from "./app/components/Screen";
-
-import React, { useState, useEffect } from "react";
-import ImageInputList from "./app/components/forms/ImageInputList";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
-  const [imageUris, setImageUris] = useState([]);
-
-  const handleAdd = (uri) => {
-    setImageUris([...imageUris, uri]);
-  };
-
-  const handleRemove = (uri) => {
-    setImageUris(imageUris.filter((imageUri) => imageUri !== uri));
-  };
-
-  return (
-    <Screen>
-      <ImageInputList
-        imageUris={imageUris}
-        onAddImage={handleAdd}
-        onRemoveImage={handleRemove}
-        // onRemoveImage={(uri) => setImageUris(imageUris.filter((imageUri) => imageUri !== uri))}
-        // onAddImage={(uri) => setImageUris([...imageUris, uri])}
-      />
-    </Screen>
-  );
+  return <ListingEditScreen />;
 }
