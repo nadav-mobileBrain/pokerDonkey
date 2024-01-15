1c36727f8e3f8f62c587742aec36aef2df57b01c
Nadav Galili <nadavg@mobile-brain.net>
2024-01-15 19:42:33 +0200

    added api

--- app/api/client.js
+++ app/api/client.js
@@ -4,7 +4,7 @@ const homeIp = "10.0.0.8";
 const workIp = "192.168.1.66";
 
 const apiClient = create({
-  baseURL: `http://${workIp}:3030`,
+  baseURL: `http://${homeIp}:3030/`,
 });
 
 export default apiClient;
--- app/screens/LeagueScreen.js
+++ app/screens/LeagueScreen.js
@@ -1,42 +1,55 @@
 import React, { useEffect, useState } from "react";
 import { StyleSheet, FlatList } from "react-native";
 
+import ActivityIndicator from "../components/ActivityIndicator";
+import AppText from "../components/AppText";
+import AppButton from "../components/AppButton";
+import apiClient from "../api/client";
 import Card from "../components/Card";
 import colors from "../config/colors";
+import leaguesApi from "../api/leagues";
 import routes from "../navigation/routes";
 import Screen from "../components/Screen";
-import leagues from "../api/leagues";
-import apiClient from "../api/client";
+import useApi from "../hooks/useApi";
 
 const serverUrl = apiClient.getBaseURL();
 
 function LeagueScreen({ navigation }) {
-  const [myLeagues, setMyLeagues] = useState([]);
-
+  const {
+    data: myLeagues,
+    error,
+    loading,
+    request: loadMyLeagues,
+  } = useApi(leaguesApi.getLeagues);
+  console.log(myLeagues.user[0].userLeagues);
   useEffect(() => {
     loadMyLeagues();
   }, []);
 
-  const loadMyLeagues = async () => {
-    const response = await leagues.getLeagues();
-
-    setMyLeagues(response?.data?.user[0]?.userLeagues);
-  };
-
   return (
     <Screen style={styles.screen}>
-      <FlatList
-        data={myLeagues}
-        keyExtractor={(league) => league.id.toString()}
-        renderItem={({ item }) => (
-          <Card
-            title={"League Name: " + item.league.league_name}
-            subTitle={"League Number " + item.league.league_number}
-            imageUrl={serverUrl + "/" + item.league.league_image}
-            onPress={() => navigation.navigate(routes.LEAGUE_DETAILS, item)}
-          />
-        )}
-      />
+      {error && (
+        <>
+          <AppText>Couldn't retrieve the leagues.</AppText>
+          <AppButton title="Retry" onPress={loadMyLeagues} />
+        </>
+      )}
+      <ActivityIndicator visible={loading} />
+
+      {myLeagues?.user[0].userLeagues.length > 0 && (
+        <FlatList
+          data={myLeagues?.user[0].userLeagues}
+          keyExtractor={(myLeague) => myLeague.id.toString()}
+          renderItem={({ item }) => (
+            <Card
+              title={item.league.league_name}
+              subTitle={item.league.league_number}
+              imageUrl={serverUrl + item.league.league_image}
+              onPress={() => navigation.navigate(routes.LEAGUE_DETAILS, item)}
+            />
+          )}
+        />
+      )}
     </Screen>
   );
 }
