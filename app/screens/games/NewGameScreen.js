281369285d642bd86664a80f815ff60ed88564d0
Nadav Galili <nadavg@mobile-brain.net>
2024-01-21 14:06:55 +0200

    Update NewGameScreen.js

--- app/screens/games/NewGameScreen.js
+++ app/screens/games/NewGameScreen.js
@@ -1,17 +1,34 @@
 import React from "react";
 import { View, StyleSheet, Text } from "react-native";
+import Screen from "../../components/Screen";
+
+import HeaderText from "../../components/HeaderText";
+import AppText from "../../components/AppText";
 
 function NewGame({ route, navigation }) {
-  console.log("🚀 ~ NewGame ~ route:", route);
+  const game = route.params.game;
+  const league = route.params.league;
+  const gameDetails = route.params.gameDetails;
+  const timeFormat = { hour: "2-digit", minute: "2-digit", second: "2-digit" };
+  const startDate = new Date(game.created_at);
+  const formattedTime = startDate.toLocaleTimeString(undefined, timeFormat);
+
   return (
-    <View style={styles.container}>
+    <Screen style={styles.container}>
+      <HeaderText>New Game</HeaderText>
+      <View style={styles.gameDetailsContainer}>
+        <AppText>Game id: {game.id}</AppText>
+        <AppText>Started At: {formattedTime}</AppText>
+      </View>
       <Text>newwew</Text>
-    </View>
+    </Screen>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    padding: 10,
+  },
 });
 
 export default NewGame;
