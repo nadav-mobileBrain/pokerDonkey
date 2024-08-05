4128a8e370cefabb9146a5cbfeaf75f2aef59028
Nadav Galili <nadavg@mobile-brain.net>
2024-08-05 16:54:46 +0300

    succcessfully google sign in

--- app/screens/forms/RegisterScreen.js
+++ app/screens/forms/RegisterScreen.js
@@ -1,5 +1,8 @@
-import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
+//ios 959357331447-a7eq0ljslvpgt642e658o3etc9s8g3vl.apps.googleusercontent.com
+///android 959357331447-6i9t65ceb4hdscse55scicb5v4vj3uud.apps.googleusercontent.com
+//web 959357331447-c7n207hsvgjj8sg1il9dufetvid1h1oa.apps.googleusercontent.com
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View , Button} from "react-native";
 import * as Yup from "yup";
 import { LinearGradient } from "expo-linear-gradient";
 
@@ -17,6 +20,13 @@ import useAuth from "../../auth/useAuth";
 import colors from "../../config/colors";
 import logger from "../../utility/logger";
 
+
+ import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from "@react-native-google-signin/google-signin";
+
 const validationSchema = Yup.object().shape({
   nickName: Yup.string().required().label("Nick Name"),
   //password: Yup.string().required().min(4).label("Password"),
@@ -29,6 +39,61 @@ const RegisterScreen = () => {
   const auth = useAuth();
   const [error, setError] = useState();
   const [imageUri, setImageUri] = useState(null); // New state for image URI
+  const [userInfo, setUserInfo] = useState(null);
+
+
+const configureGoogleSignin = () => {
+  GoogleSignin.configure({
+    webClientId: '959357331447-c7n207hsvgjj8sg1il9dufetvid1h1oa.apps.googleusercontent.com',
+   androidClientId:"959357331447-6i9t65ceb4hdscse55scicb5v4vj3uud.apps.googleusercontent.com",
+   iosClientId:"959357331447-a7eq0ljslvpgt642e658o3etc9s8g3vl.apps.googleusercontent.com"
+  });
+}
+
+useEffect(() => {
+  configureGoogleSignin();
+}, []);
+const signIn = async () => {
+  console.log("Pressed sign in");
+
+  try {
+    await GoogleSignin.hasPlayServices();
+    const userInfo = await GoogleSignin.signIn();
+    console.log("🚀 ~ signIn ~ userInfo:", userInfo.user)
+    setUserInfo(userInfo);
+    //setError();
+  } catch (e) {
+   console.log("🚀 ~ signIn ~ e:", e)
+   // setError(e);
+  }
+};
+
+// const signIn =async () =>{
+//   console.log("sign in");
+
+//   try {
+//     await GoogleSignin.askForPlayServicesAsync();
+//     const { type, user } = await GoogleSignin.signInAsync();
+//     if (type === 'success') {
+//       console.log('user',user);
+//       const userInfo = {
+//         nickName: user.name,
+//         email: user.email,
+//         image: user.photoUrl,
+//       };
+//       console.log('userinf',userInfo);
+    
+    
+      
+//     }
+//   }catch (error) {
+//     console.log(error);
+//   }
+// }
+    
+
+
+
 
   const handleSubmit = async (userInfo) => {
     const completeUserInfo = {
@@ -89,6 +154,8 @@ const RegisterScreen = () => {
               *You can add/change your image later
             </AppText>
           </View>
+          {userInfo && <AppText>{JSON.stringify(userInfo.user)}</AppText>}
+          <GoogleSigninButton size={GoogleSigninButton.Size.Wide} color={GoogleSigninButton.Color.Dark} onPress={signIn}/>
           <SubmitButton title="Register" icon="account-plus" color="gold"/>
         </AppForm>
       </LinearGradient>
